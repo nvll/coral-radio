c9872920010e14270c25d2234ccbdae10da0da70
Christopher Anderson <chris@nullcode.org>
2016-04-17 16:33:32 -0700

    Move lib/nrf_ble to lib/s130

--- lib/s130/module.mk
+++ lib/s130/module.mk
@@ -2,21 +2,12 @@
 # services for BLE support. Anything unused is stripped by the
 # linker.
 
-MODULES += \
-	lib/nrf_drv/pstorage
-
 MODULE_SOURCES += \
-	third_party/nrf51_sdk/components/ble/ble_advertising/ble_advertising.c \
 	third_party/nrf51_sdk/components/ble/common/ble_advdata.c \
 	third_party/nrf51_sdk/components/ble/common/ble_conn_params.c \
-    third_party/nrf51_sdk/components/ble/common/ble_conn_state.c \
+    	third_party/nrf51_sdk/components/ble/common/ble_conn_state.c \
 	third_party/nrf51_sdk/components/ble/common/ble_srv_common.c \
-	third_party/nrf51_sdk/components/libraries/trace/app_trace.c \
-	third_party/nrf51_sdk/components/libraries/timer/app_timer.c \
 	third_party/nrf51_sdk/components/softdevice/common/softdevice_handler/softdevice_handler.c \
 
 MODULE_INCLUDES += \
-	third_party/nrf51_sdk/components/ble/ble_advertising \
 	third_party/nrf51_sdk/components/ble/common \
-	third_party/nrf51_sdk/components/libraries/trace \
-	third_party/nrf51_sdk/components/libraries/timer \
